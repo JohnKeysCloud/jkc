307ff94acc59a0acb4e14bfba81b0a57ff1bd013
johnKeysCloud <johncloud@cyclonestud.io>
2022-07-21 17:11:12 -0400

    start adding javascript for modals

--- js/script.js
+++ js/script.js
@@ -1,17 +1,37 @@
-const openModal = document.querySelector('.gif-container');
+const openModal = document.getElementsByClassName('modal-trigger')[0];
 const closeModal = document.querySelector('.close-btn')
 let modalOpen = false;
 
 openModal.addEventListener('click', () => {
-  if(!modalOpen) {
+  if (!modalOpen) {
     openModal.classList.add('open');
     modalOpen = true;
-    }
+  } else {
+    openModal.classList.remove('open');
+    modalOpen = false;
+  }
+}); 
+
+
+const openBtnContainer = document.getElementsByClassName("modal-trigger")[1];
+
+openBtnContainer.addEventListener("click", () => {
+  if (!modalOpen) {
+    openBtnContainer.classList.add("open");
+    modalOpen = true;
+  }
 }); 
 
 closeModal.addEventListener('click', () => {
   if (modalOpen) {
-    openModal.classList.remove('open');
+    openBtnContainer.classList.remove("open");
     modalOpen = false;
   }
 });
+
+
+
+// Start to target all items within array
+// for (var i = 0; i < openModal.length; i++) {
+//   openModal[i].classList.add('open');
+// };
