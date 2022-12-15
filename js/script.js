68d6264abd9715d193b33c0d9fca19779688ad5e
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2022-12-15 02:11:31 -0500

    adjust bio

--- js/script.js
+++ js/script.js
@@ -66,54 +66,6 @@ document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
   });
 });
 
-
-const pikaNumberText = document.getElementById('pikaNumber-text');
-let projectMobileVisual = document.getElementsByClassName("projectImgContainer")[0];
-let linkContainer = document.getElementsByClassName("link-container")[0];
-let cloudClose = document.getElementsByClassName("cyClose-btn")[0];
-let pikaNumberGif = document.getElementById('pikaNumber-gif');
-
-pikaNumberText.addEventListener('click', projectToggle);
-
-function projectToggle() {
-
-  pikaNumberGif.style.setProperty("filter", "saturate(0)");
-
-  pikaNumberText.style.setProperty('opacity', '0.5');
-  pikaNumberText.style.setProperty('cursor', 'initial');
-  
-  projectMobileVisual.style.setProperty('z-index', '1');
-  projectMobileVisual.style.setProperty("transform", "translate(-50%, -50%) scale(1.4) rotate(0deg)");
-
-  cloudClose.style.setProperty('opacity', '1');
-  cloudClose.style.setProperty('visibility', 'visible');
-
-  linkContainer.style.setProperty('opacity', '1');
-  linkContainer.style.setProperty('visibility', 'visible');
-
-  document.documentElement.style.setProperty('--js-opacity', '1');
-  document.documentElement.style.setProperty('--project-filter', 'drop-shadow(0 11px 11px var(--clr-binky))');
-
-  cloudClose.addEventListener('click', () => {
-    pikaNumberGif.style.setProperty("filter", "saturate(1)");
-
-    pikaNumberText.style.setProperty("opacity", "1");
-    pikaNumberText.style.setProperty("cursor", "pointer");
-
-    projectMobileVisual.style.setProperty("z-index", "0");
-    projectMobileVisual.style.setProperty("transform", "translate(-50%, -50%) scale(1.2) rotate(13deg)");
-    
-    cloudClose.style.setProperty('opacity', '0');
-    cloudClose.style.setProperty("visibility", "hidden");
-
-    linkContainer.style.setProperty("opacity", "0");
-    linkContainer.style.setProperty("visibility", "hidden");
-
-    document.documentElement.style.setProperty("--js-opacity", "0");
-    document.documentElement.style.setProperty('--project-filter', 'drop-shadow(none)');
-  });
-};
-
 // const width  = window.innerWidth || document.documentElement.clientWidth || 
 // document.body.clientWidth;
 // const height = window.innerHeight|| document.documentElement.clientHeight|| 
