2d40e198469ede40ba65ac03321755c8a36a1758
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2022-12-20 02:23:53 -0500

    clean js a bit

--- js/script.js
+++ js/script.js
@@ -1,70 +1,57 @@
+const anchors = document.querySelectorAll('a[href^="#"]');
+const inception = document.querySelector('.project-visual');
 const key = document.getElementById('key-img')
-const openModal = document.querySelector('.nav-toggle');
-let navOpen = false;
-key.addEventListener('click', () => {
-  if (!navOpen) {
-    openModal.classList.add('open');
-    navOpen = true;
-  } else {
-    openModal.classList.remove('open');
-    navOpen = false;
-  }
-}); 
-
-
 const modalTriggerBtn = document.querySelector('#tapMeContainer');
-const inception = document.querySelector(".project-visual");
-let modalOpen = false;
-
-modalTriggerBtn.addEventListener("click", () => {
-  if (!modalOpen) {
-    openBtnContainer.classList.add("open");
-    inception.classList.add("inception");
-    tapMe.classList.add("tapped");
-    modalOpen = true;
-  }
-}); 
-
-const openBtnContainer = document.querySelector(".modal-trigger");
-const tapMe = document.querySelector("#levelOne");
-const closeModal = document.querySelector(".close-btn");
-
-
-openBtnContainer.addEventListener("click", () => {
-  if (!modalOpen) {
-    openBtnContainer.classList.add("open");
-    tapMe.classList.add("tapped")
-    inception.classList.add("inception");
-    modalOpen = true;
-  }
-}); 
-closeModal.addEventListener('click', () => {
-  if (modalOpen) {
-    openBtnContainer.classList.remove("open");
-    tapMe.classList.remove("tapped");
-    inception.classList.remove("inception");
-    modalOpen = false;
-  }
-});
-
-const stickyCheck = document.querySelector("#navigation");
+const nav = document.querySelector("#navigation");
+const navModal = document.querySelector('.nav-toggle');
 const observer = new IntersectionObserver(
   ([e]) => {
-    return e.target.classList.toggle("is-pinned", e.intersectionRatio < 1);
+    return e.target.classList.toggle('is-pinned', e.intersectionRatio < 1);
   },
   { threshold: [1] }
 );
-observer.observe(stickyCheck);
+const openBtnContainer = document.querySelector('.modal-trigger');
+const tapMe = document.querySelector('#levelOne');
+const tcnyModalCloseButton = document.querySelector('.close-btn');
+let modalOpen = false;
 
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener("click", function (e) {
+function toggleNav() {
+  navModal.classList.toggle('open');
+}
+key.addEventListener('click', toggleNav); 
+
+function initiateLevelOne() {
+  if (!modalOpen) {
+    openBtnContainer.classList.add('open');
+    inception.classList.add('inception');
+    tapMe.classList.add('tapped');
+    modalOpen = true;
+  }
+}
+modalTriggerBtn.addEventListener("click", initiateLevelOne);
+openBtnContainer.addEventListener("click", initiateLevelOne);
+
+function deactivateLevelOne() {
+  if (modalOpen) {
+    openBtnContainer.classList.remove('open');
+    tapMe.classList.remove('tapped');
+    inception.classList.remove('inception');
+    modalOpen = false;
+  }
+}
+tcnyModalCloseButton.addEventListener('click', deactivateLevelOne);
+
+anchors.forEach((anchor) => {
+  anchor.addEventListener("click", function(e) {
     e.preventDefault();
-
+    
     document.querySelector(this.getAttribute("href")).scrollIntoView({
       behavior: "smooth",
     });
   });
 });
+
+observer.observe(nav);
 
 // const width  = window.innerWidth || document.documentElement.clientWidth || 
 // document.body.clientWidth;
