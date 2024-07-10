2832d74a5784997da87c3ce84d9caf8023838375
JOHN KEYS CLOUD <90482169+JohnKeysCloud@users.noreply.github.com>
2024-07-10 01:41:29 -0400

    refactor js

--- js/script.js
+++ js/script.js
@@ -1,59 +1,101 @@
-const anchors = document.querySelectorAll('a[href^="#"]');
-const inception = document.querySelector('.project-visual');
-const key = document.getElementById('key-img')
-const modalTriggerBtn = document.querySelector('#tapMeContainer');
-const nav = document.querySelector("#navigation");
-const navButtons = document.querySelectorAll('nav li');
-const navModal = document.querySelector('.nav-toggle');
-const openBtnContainer = document.querySelector('.modal-trigger');
-const tapMe = document.querySelector('#levelOne');
-const tcnyModalCloseButton = document.querySelector('.close-btn');
-const footerYear = document.getElementById('footer-year');
-const currentYear = new Date().getFullYear();
+const getElement = (selector, scope = document) => scope.querySelector(selector);
+const getAllElements = (selector, scope = document) => scope.querySelectorAll(selector);
+const addListener = (element, event, handler) => element.addEventListener(event, handler);
+const removeListener = (element, event, handler) => element.removeEventListener(event, handler);
+const toggleClass = (element, action, className) => element.classList[action](className);
+const setDisabledState = (element, state) => element.disabled = state;
+
+// 💭 --------------------------------------------------------------
+
+const elementSet = {
+	withListeners: {
+		anchors: () => getAllElements('a[href ^= "#"]'),
+		navKeyButton: () => getElement('#nav-key-button'),
+		navListItems: () => getAllElements('.nav-list-item'),
+		tcnyLevelOneCloseButton: () => getElement('#tcny-level-one-close-button'),
+		tcnyTapHereContainer: () => getElement('#tcny-tap-here-container'),
+		tcnyWidgetVisualContainer: () => getElement('#tcny-widget-visual-container'),
+	},
+	withoutListeners: {
+		footerYear: () => getElement('#footer-year'),
+		nav: () => getElement('#nav'),
+		tcnyWidgetContainer: () => getElement('#tcny-widget-container'),
+		tcnyWidgetTextContainer: () => getElement('#tcny-widget-text-container'),
+	}
+}
+
+// 💭 --------------------------------------------------------------
+  
+function toggleLevelOne() {
+  elementSet.withoutListeners.tcnyWidgetContainer().classList.toggle('active');
+}
 
 function toggleNav() {
-  navModal.classList.toggle('open');
+	elementSet.withListeners.navKeyButton().classList.toggle('engaged');
 }
 
-function initiateLevelOne() {
-    openBtnContainer.classList.toggle('open');
-    inception.classList.toggle('inception');
-    tapMe.classList.toggle('tapped');
+function smoothScoll(e) {
+	e.preventDefault();
+
+	document.querySelector(this.getAttribute("href")).scrollIntoView({
+		behavior: "smooth",
+	});
 }
 
-function deactivateLevelOne() {
-    openBtnContainer.classList.toggle('open');
-    tapMe.classList.toggle('tapped');
-    inception.classList.toggle('inception');
+// 💭 --------------------------------------------------------------
+
+function addListeners() {
+	elementSet.withListeners.navKeyButton().addEventListener('click', toggleNav);
+	elementSet.withListeners.tcnyTapHereContainer().addEventListener('click', toggleLevelOne);
+	elementSet.withListeners.tcnyWidgetVisualContainer().addEventListener('click', toggleLevelOne);
+	elementSet.withListeners.tcnyLevelOneCloseButton().addEventListener('click', toggleLevelOne);
+	elementSet.withListeners.navListItems().forEach((listItem) =>
+		listItem.addEventListener('click', toggleNav)
+	);
+	elementSet.withListeners.anchors().forEach((anchor) => 
+		anchor.addEventListener("click", smoothScoll));
 }
 
-const observer = new IntersectionObserver(
-  ([e]) => {
-    return e.target.classList.toggle('is-pinned', e.intersectionRatio < 1);
-  },
-  { threshold: [1] }
-);
+function attachKeyObserver() {
+	const observer = new IntersectionObserver(
+		([e]) => {
+			return e.target.classList.toggle('is-pinned', e.intersectionRatio < 1);
+		},
+		{ threshold: [1] }
+	);
 
-observer.observe(nav);
+	observer.observe(elementSet.withoutListeners.nav());
+}
 
-// ? scrolls section into view smoothly
-anchors.forEach((anchor) => {
-  anchor.addEventListener("click", function(e) {
-    e.preventDefault();
-    
-    document.querySelector(this.getAttribute("href")).scrollIntoView({
-      behavior: "smooth",
-    });
-  });
+function addDynamicYear() {
+	const currentYear = new Date().getFullYear();
+	elementSet.withoutListeners.footerYear().innerHTML = currentYear;
+}
+
+// 💭 --------------------------------------------------------------
+
+function init() {
+	addDynamicYear();
+	attachKeyObserver();
+	addListeners();
+}
+
+// 💭 --------------------------------------------------------------
+
+document.addEventListener('DOMContentLoaded', () => {
+	init();
 });
 
-// ? resets key on nav anchor click
-navButtons.forEach((button) =>
-  button.addEventListener('click', () => key.click())
-);
+// // window.onload = () => {
+// // 	console.log('All resources finished loading!');
+// // 	init();
+// // };
 
-footerYear.innerHTML = currentYear;
-key.addEventListener('click', toggleNav); 
-openBtnContainer.addEventListener('click', initiateLevelOne);
-modalTriggerBtn.addEventListener('click', initiateLevelOne);
-tcnyModalCloseButton.addEventListener('click', deactivateLevelOne);
+/* 
+	┌─────────────────────────────────────────────────────────────────────────┐
+	│ In most cases, you would use `DOMContentLoaded` as it ensures that your │
+	│ script runs as soon as the DOM is ready, without waiting for the        │
+	│ entire page to load, which can improve the perceived performance of     │
+	│ your application. `window.onload` waits for all to load (styles/imgs).  │
+	└─────────────────────────────────────────────────────────────────────────┘
+ */
